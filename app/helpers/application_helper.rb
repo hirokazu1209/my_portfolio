8561699e4b240381e0ff206e17ccf10d8643fc6c
hirokazu1209 <n.hirokazu92@gmail.com>
2025-01-02 10:26:41 +0900

    add: エラーメッセージをフォーム順に表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def custom_order(message)
+    order = {
+      "名前" => 1,
+      "メールアドレス" => 2,
+      "パスワード" => 3,
+    }
+    # メッセージの最初の部分に基づいて順序を決定
+    order.each { |key, value| return value if message.start_with?(key) }
+    999 # その他のメッセージは最後に表示
+  end
 end
