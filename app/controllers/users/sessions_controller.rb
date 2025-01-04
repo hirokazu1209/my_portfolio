b1f761045683d218c84210a0f4a1047c1b31b072
hirokazu1209 <n.hirokazu92@gmail.com>
2025-01-04 23:31:16 +0900

    fix: 不要なファイルの削除・after_sign_out_path_forメソッドをapplicationコントローラに記述

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-  protected
-
-  # ログアウト後のリダイレクト先を変更
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
