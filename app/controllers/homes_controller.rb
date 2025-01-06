8ac2e6e6aa6ac00d6237ec7e40d2c2193c0611b7
hirokazu1209 <n.hirokazu92@gmail.com>
2025-01-06 22:16:16 +0900

    feat: 編集機能の処理を追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,4 +4,23 @@ class HomesController < ApplicationController
   def top
     @user = current_user
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :self_introduction, :profile_picture_pass)
+  end
 end
