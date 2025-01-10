2ea413a408d6dea303a6105996af835c8306da01
hirokazu1209 <n.hirokazu92@gmail.com>
2025-01-10 22:53:45 +0900

    add: プロフィール画像アップロードの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:self_introduction, :profile_picture_pass])
   end
 
   def after_sign_out_path_for(resource_or_scope)
