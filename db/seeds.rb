5ed551bff4f5617f58b93bb16c508720285f48d6
hirokazu1209 <n.hirokazu92@gmail.com>
2025-01-20 23:25:26 +0900

    add: seedファイルの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'factory_bot_rails'
+
+if Rails.env.production?
+  puts '本番環境では seed を実行しません'
+  exit
+end
+
+Category.create([
+  { name: 'バックエンド' },
+  { name: 'フロントエンド' },
+  { name: 'インフラ' }
+])
+
+FactoryBot.create(:user)
+
+Category.all.each do |category|
+  # 3.times文を使って、当月・1ヶ月前・2ヶ月前のデータを作成する
+  3.times do |n|
+    Item.create(
+      name: "#{category.name}のタイトル#{n + 1}",
+      study_time: 60,
+      user_id: 1,
+      category_id: category.id,
+      created_at: n.month.ago
+    )
+  end
+
+  if category.errors.any?
+    puts "#{category.name}: #{category.errors.full_messages.join(', ')}"
+  end
+end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { "test1234" }
+    self_introduction { Faker::Lorem.characters(number: 60) }
   end
 end
